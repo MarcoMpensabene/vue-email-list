50e8cdb3d0bce96b34c899a3b110c1859a7b1d41
MarcoMpensabene <marcompensabene@gmail.com>
2024-06-05 15:43:00 +0200

    changed arg for the fuction in "mail"

--- js/style.js
+++ js/style.js
@@ -10,8 +10,8 @@ createApp({
         getData(){
             for (let i = 0; i < 10; i++) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((response) => {
-                    this.randomArrayMail.push(response.data.response);
+            .then((mail) => {
+                    this.randomArrayMail.push(mail.data.response);
                 })
             }
         }
